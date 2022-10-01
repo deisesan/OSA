525cea9ccb653a80cc556f63455c641a26fb621a
Dayany Lima Santos <dayanyylima@gmail.com>
2022-10-01 12:44:34 -0300

    Update

--- Atividade Prática 2/funcoes.cpp
+++ Atividade Prática 2/funcoes.cpp
@@ -49,7 +49,7 @@ void IndiceDireto(string nomeArquivo)
 {
     Netflix registro;
     ofstream output;
-    char buffer[1000];
+    char buffer[1500];
     int tam, cabecalho = 0;
     long endereco;
 
@@ -63,7 +63,7 @@ void IndiceDireto(string nomeArquivo)
         {
             input.read((char *)&tam, sizeof(int));
             endereco = input.tellg();
-            cout << endereco << endl;
+            // cout << endereco << endl;
             input.read(buffer, tam);
 
             if (cabecalho == 0)
@@ -73,7 +73,10 @@ void IndiceDireto(string nomeArquivo)
             else
             {
                 registro.split(buffer);
-                output << registro.getShowId() << "|" << endereco << endl;
+                if (registro.getShowId() != "")
+                {
+                    output << registro.getShowId() << "|" << endereco << endl;
+                }
             }
 
             buffer[0] = '\0';
@@ -82,4 +85,85 @@ void IndiceDireto(string nomeArquivo)
     }
 
     output.close();
+}
+
+void pesquisa(string nomeArquivo)
+{
+    Netflix registro;
+    bool found = false;
+    string line;
+    string str_pesquisa, aux;
+    int opcao;
+
+    ifstream input(nomeArquivo);
+    do
+    {
+        cout << "Deseja fazer pesquisa por Show_id(digite 1) ou Titulo(digite 2): " << endl;
+        cin >> opcao;
+
+    } while (opcao != 1 && opcao != 2);
+
+    if (opcao == 1)
+    {
+
+        cout << "Digite Show_id que deseja pesquisar: ";
+        cin >> str_pesquisa;
+    }
+    else
+    {
+        cout << "Digite titulo que deseja procurar: ";
+        cin.ignore();
+        getline(cin, str_pesquisa);
+    }
+
+    while (!input.eof())
+    {
+        getline(input, line);
+
+        registro.split(line);
+
+        if (opcao == 1)
+        {
+
+            aux = registro.getShowId();
+
+            for_each(aux.begin(), aux.end(), [](char &c)
+                     { c = ::toupper(c); });
+
+            for_each(str_pesquisa.begin(), str_pesquisa.end(), [](char &c)
+                     { c = ::toupper(c); });
+
+            if (aux.compare(str_pesquisa) == 0)
+            {
+                found = true;
+                cout << " ---> RESULTADO ENCONTRADO: " << endl;
+                registro.print();
+            }
+        }
+        else if (opcao == 2)
+        {
+            aux = registro.getTitle();
+
+            for_each(aux.begin(), aux.end(), [](char &c)
+                     { c = ::toupper(c); });
+
+            for_each(str_pesquisa.begin(), str_pesquisa.end(), [](char &c)
+                     { c = ::toupper(c); });
+
+            if (aux.find(str_pesquisa) != std::string::npos)
+            {
+                found = true;
+                cout << "---> RESULTADO ENCONTRADO: " << endl;
+                registro.print();
+            }
+        }
+
+        aux.clear();
+        line.clear();
+        registro.clear();
+    }
+    if (!found)
+    {
+        cout << "NENHUM RESULTADO ENCONTRADO: " << endl;
+    }
 }
--- Atividade Prática 2/main.cpp
+++ Atividade Prática 2/main.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
-    string nomeArquivo = "netflix_titles1.csv";
-    string nomeArquivoDat = "netflix_titles1.dat";
+    string nomeArquivo = "netflix_titles.csv";
+    string nomeArquivoDat = "netflix_titles.dat";
 
     Arquivo(nomeArquivo);
     IndiceDireto(nomeArquivoDat);
+
+    //pesquisa(nomeArquivo);
 
     return 0;
 }
